572d7732ca4b462c7e68de31517f76bdd0151028
unknown <kamal.berriga@gmail.com>
2020-03-30 17:09:07 +0200

    ino

---
+++ inovlend/js/tabPane.js
@@ -0,0 +1,79 @@
+// script for tab steps
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+
+        var href = $(e.target).attr('href');
+        var $curr = $(".process-model  a[href='" + href + "']").parent();
+
+        $('.process-model li').removeClass();
+
+        $curr.addClass("active");
+        $curr.prevAll().addClass("visited");
+    });
+// end  script for tab steps
+$(function(){
+ $('.btn-circle').on('click',function(){
+   $('.btn-circle.btn-info').removeClass('btn-info').addClass('btn-default');
+   $(this).addClass('btn-info').removeClass('btn-default').blur();
+ });
+
+ $('.next-step, .prev-step').on('click', function (e){
+   var $activeTab = $('.tab-pane.active');
+
+   $('.btn-circle.btn-info').removeClass('btn-info').addClass('btn-default');
+
+   if ( $(e.target).hasClass('next-step') )
+   {
+      var nextTab = $activeTab.next('.tab-pane').attr('id');
+      $('[href="#'+ nextTab +'"]').addClass('btn-info').removeClass('btn-default');
+      $('[href="#'+ nextTab +'"]').tab('show');
+   }
+   else
+   {
+      var prevTab = $activeTab.prev('.tab-pane').attr('id');
+      $('[href="#'+ prevTab +'"]').addClass('btn-info').removeClass('btn-default');
+      $('[href="#'+ prevTab +'"]').tab('show');
+   }
+ });
+ 
+});
+
+
+jQuery(document).ready(function() {
+	
+    /*
+        Fullscreen background
+    */
+  
+    
+    /*
+	    Modals
+	*/
+	$('.launch-modal').on('click', function(e){
+		e.preventDefault();
+		$( '#' + $(this).data('modal-id') ).modal();
+	});
+    
+    /*
+        Form validation
+    */
+    $('.registration-form input[type="text"], .registration-form textarea').on('focus', function() {
+    	$(this).removeClass('input-error');
+    });
+    
+    $('.registration-form').on('submit', function(e) {
+    	
+    	$(this).find('input[type="text"], textarea').each(function(){
+    		if( $(this).val() == "" ) {
+    			e.preventDefault();
+    			$(this).addClass('input-error');
+    		}
+    		else {
+    			$(this).removeClass('input-error');
+    		}
+    	});
+    	
+    });
+    
+    
+});
+
